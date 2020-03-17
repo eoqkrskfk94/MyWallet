7f8691b7497fc13d9b083bb32399b91ecb5d5fdc
mjkim <eoqkrskfk94@naver.com>
2020-03-17 17:22:51 +0900

    start project

--- app/src/main/java/com/wallet1000/HelloWorld.kt
+++ app/src/main/java/com/wallet1000/HelloWorld.kt
@@ -4,7 +4,7 @@ fun main(){
     val hello1: String = "Hello World!" //final
     var hello2 = "Hello World!" // int,string,float,double
 
-    hello2 = "Hello World!"
+    hello2 = "Hello World!!"
     hello()
 
     println(hello2)
