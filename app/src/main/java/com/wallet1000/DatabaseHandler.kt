29b94cdda9fb36d22b03d0f64ac532d1c909d14d
김명진 <eoqrkskfk94@naver.com>
2020-03-18 23:28:29 +0900

    3rd try

--- app/src/main/java/com/wallet1000/DatabaseHandler.kt
+++ app/src/main/java/com/wallet1000/DatabaseHandler.kt
@@ -1,4 +1,26 @@
 package com.wallet1000
 
-class DatabaseHandler {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+val DATABASE_NAME = "MyDB"
+val TABLE_NAME = "User"
+val COL_ID = "ID"
+val COL_MONEY = "money"
+
+class DatabaseHandler(context: Context):SQLiteOpenHelper(context,DATABASE_NAME,null,1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        val createTable = "CREATE TABLE " + TABLE_NAME + " (" +
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_MONEY + " INTEGER)"
+
+        db?.execSQL(createTable)
+
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
