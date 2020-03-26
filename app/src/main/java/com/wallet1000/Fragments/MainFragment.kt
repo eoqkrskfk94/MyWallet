e3361ffe4734d55081e02d26f74370a204b9b4a3
mjkim <eoqkrskfk94@naver.com>
2020-03-26 17:33:16 +0900

    buttons

--- app/src/main/java/com/wallet1000/Fragments/MainFragment.kt
+++ app/src/main/java/com/wallet1000/Fragments/MainFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import com.wallet1000.DatabaseHandler
@@ -31,47 +32,86 @@ class MainFragment : Fragment(){
         super.onViewCreated(view, savedInstanceState)
 
         val context: Context = context!!
-        var button10000 = view.findViewById<ImageButton>(R.id.btn10000)
-        var button1000 = view.findViewById<ImageButton>(R.id.btn1000)
-        var button100 = view.findViewById<ImageButton>(R.id.btn100)
+        var button10000_up = view.findViewById<ImageButton>(R.id.right10000)
+        var button10000_down = view.findViewById<ImageButton>(R.id.left10000)
+        var button1000_up = view.findViewById<ImageButton>(R.id.right1000)
+        var button1000_down = view.findViewById<ImageButton>(R.id.left1000)
+        var button100_up = view.findViewById<ImageButton>(R.id.right100)
+        var button100_down = view.findViewById<ImageButton>(R.id.left100)
         var buttonSave = view.findViewById<ImageButton>(R.id.btn_save)
+        var buttonDelete = view.findViewById<ImageButton>(R.id.btn_delete)
         var total = view.findViewById<TextView>(R.id.currentMoney)
+        var now_date = view.findViewById<TextView>(R.id.date_text)
+        var now_time = view.findViewById<TextView>(R.id.time_text)
         var db = DatabaseHandler(context)
 
-        button10000.setOnClickListener{
+        val now = LocalDateTime.now()
+        var date = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        var time = DateTimeFormatter.ofPattern("HH:MM")
+        val dateString: String = date.format(now)
+        val timeString: String = time.format(now)
+
+        now_date.text = "123"
+        //now_time.text = "32323"
+
+        total.text = "₩$money"
+
+        //10000만원
+        button10000_up.setOnClickListener{
             money += 10000
             if (money < 0){ total.text = "0" }
-            else{ total.text = "$money" }
+            else{ total.text = "₩$money" }
         }
 
-        button1000.setOnClickListener{
+        button10000_down.setOnClickListener{
+            money -= 10000
+            if (money < 0){ total.text = "0" }
+            else{ total.text = "₩$money" }
+        }
+
+        //1000원
+        button1000_up.setOnClickListener{
             money += 1000
             if (money < 0){ total.text = "0" }
-            else{ total.text = "$money" }
+            else{ total.text = "₩$money" }
         }
 
-        button100.setOnClickListener{
+        button1000_down.setOnClickListener{
+            money -= 1000
+            if (money < 0){ total.text = "0" }
+            else{ total.text = "₩$money" }
+        }
+
+        //100원
+        button100_up.setOnClickListener{
             money += 100
             if (money < 0){ total.text = "0" }
-            else{ total.text = "$money" }
+            else{ total.text = "₩$money" }
         }
 
+        button100_down.setOnClickListener{
+            money -= 100
+            if (money < 0){ total.text = "0" }
+            else{ total.text = "₩$money" }
+        }
+
+        //등록하기 버튼
         buttonSave.setOnClickListener{
-            val now = LocalDateTime.now()
-            var date = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-            var time = DateTimeFormatter.ofPattern("HH:MM")
-
-            val dateString: String = date.format(now)
-            val timeString: String = time.format(now)
-
 
             if (money.toString().length > 0){
                 var user = User(money,dateString,timeString)
                 db.insertData(user)
             }
             money = 0
-            total.text = "$money"
+            total.text = "₩$money"
         }
+
+        //삭제하기 버튼
+        buttonDelete.setOnClickListener{
+            money = 0
+            total.text = "₩$money"
+        }
+
     }
 
 
