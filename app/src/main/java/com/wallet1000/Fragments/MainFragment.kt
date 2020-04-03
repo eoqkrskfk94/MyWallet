7f1723f092e628811043c99ecbaefb2dffee5214
김명진 <eoqrkskfk94@naver.com>
2020-04-03 20:37:34 +0900

    kotlin style button and refresh button

--- app/src/main/java/com/wallet1000/Fragments/MainFragment.kt
+++ app/src/main/java/com/wallet1000/Fragments/MainFragment.kt
@@ -32,14 +32,7 @@ class MainFragment : Fragment(){
         super.onViewCreated(view, savedInstanceState)
 
         val context: Context = context!!
-        var button10000_up = view.findViewById<ImageButton>(R.id.right10000)
-        var button10000_down = view.findViewById<ImageButton>(R.id.left10000)
-        var button1000_up = view.findViewById<ImageButton>(R.id.right1000)
-        var button1000_down = view.findViewById<ImageButton>(R.id.left1000)
-        var button100_up = view.findViewById<ImageButton>(R.id.right100)
-        var button100_down = view.findViewById<ImageButton>(R.id.left100)
-        var buttonSave = view.findViewById<ImageButton>(R.id.btn_save)
-        var buttonDelete = view.findViewById<ImageButton>(R.id.btn_delete)
+
         var total = view.findViewById<TextView>(R.id.currentMoney)
         var now_date = view.findViewById<Button>(R.id.textdate)
         var now_time = view.findViewById<Button>(R.id.texttime)
@@ -47,58 +40,69 @@ class MainFragment : Fragment(){
 
 
 
-        val now = LocalDateTime.now()
+        var now = LocalDateTime.now()
         var date = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         var time = DateTimeFormatter.ofPattern("HH:MM")
-        val dateString: String = date.format(now)
-        val timeString: String = time.format(now)
+        var dateString: String = date.format(now)
+        var timeString: String = time.format(now)
 
         now_date.text = "$dateString"
         now_time.text = "$timeString"
 
         total.text = "₩$money"
 
+        refreshButton.setOnClickListener{
+            now = LocalDateTime.now()
+            date = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+            time = DateTimeFormatter.ofPattern("HH:MM")
+            dateString = date.format(now)
+            timeString = time.format(now)
+
+            now_date.text = "$dateString"
+            now_time.text = "$timeString"
+        }
+
         //10000만원
-        button10000_up.setOnClickListener{
+        right10000.setOnClickListener{
             money += 10000
             if (money < 0){ total.text = "0" }
             else{ total.text = "₩$money" }
         }
 
-        button10000_down.setOnClickListener{
+        left10000.setOnClickListener{
             money -= 10000
             if (money < 0){ total.text = "0" }
             else{ total.text = "₩$money" }
         }
 
         //1000원
-        button1000_up.setOnClickListener{
+        right1000.setOnClickListener{
             money += 1000
             if (money < 0){ total.text = "0" }
             else{ total.text = "₩$money" }
         }
 
-        button1000_down.setOnClickListener{
+        left1000.setOnClickListener{
             money -= 1000
             if (money < 0){ total.text = "0" }
             else{ total.text = "₩$money" }
         }
 
         //100원
-        button100_up.setOnClickListener{
+        right100.setOnClickListener{
             money += 100
             if (money < 0){ total.text = "0" }
             else{ total.text = "₩$money" }
         }
 
-        button100_down.setOnClickListener{
+        left100.setOnClickListener{
             money -= 100
             if (money < 0){ total.text = "0" }
             else{ total.text = "₩$money" }
         }
 
         //등록하기 버튼
-        buttonSave.setOnClickListener{
+        btn_save.setOnClickListener{
 
             if (money.toString().length > 0){
                 var user = User(money,dateString,timeString)
@@ -109,7 +113,7 @@ class MainFragment : Fragment(){
         }
 
         //삭제하기 버튼
-        buttonDelete.setOnClickListener{
+        btn_delete.setOnClickListener{
             money = 0
             total.text = "₩$money"
         }
